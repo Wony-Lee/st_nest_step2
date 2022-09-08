5c22b003a1db4f7742f232e6d2f2a396383f699c
lyw93152301@gmail.com <a1_11_11@naver.com>
2022-09-08 20:08:08 +0900

    repository set

--- src/tasks/TasksRepository.ts
+++ src/tasks/TasksRepository.ts
@@ -1,6 +1,20 @@
 import { Repository } from 'typeorm';
 import { Task } from './entities/task.entity';
 import { CustomRepository } from '../databases/typeorm-ex.decorator';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
 
 @CustomRepository(Task)
-export class TasksRepository extends Repository<Task> {}
+export class TasksRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+    await this.save(task);
+    return task;
+  }
+}
