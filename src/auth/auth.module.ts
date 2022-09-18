b317868b7998736a294becad0a016ea73a78fbc9
lyw93152301@gmail.com <a1_11_11@naver.com>
2022-09-19 01:28:11 +0900

    auth jwt update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,24 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeormExModule } from '../databases/typeorm-ex.module';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeormExModule.forCustomRepository([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('SECRET_KEY'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+    TypeormExModule.forCustomRepository([UsersRepository]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import { CustomRepository } from '../databases/typeorm-ex.decorator';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 import {
   ConflictException,
   InternalServerErrorException,
@@ -12,7 +13,12 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredential: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredential;
 
-    const user = this.create({ username, password });
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (e) {
