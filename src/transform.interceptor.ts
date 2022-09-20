4f04f453257e7cd6bb36ea5b78dda7ac7db34c03
lyw93152301@gmail.com <a1_11_11@naver.com>
2022-09-20 23:29:25 +0900

    user auth update

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,15 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map } from 'rxjs';
+import { instanceToPlain } from 'class-transformer';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>) {
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  }
+}
